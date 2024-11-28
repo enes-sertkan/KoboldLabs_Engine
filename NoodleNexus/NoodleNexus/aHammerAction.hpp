504b5fe72d3455bdcb2dec4c4c9c073da4399406
VasukaLabs <vasukaman@gmail.com>
2024-11-28 03:12:00 -0400

    *

--- NoodleNexus/NoodleNexus/aHammerAction.hpp
+++ NoodleNexus/NoodleNexus/aHammerAction.hpp
@@ -2,12 +2,15 @@
 
 #include "Action.h"
 #include "Scene.hpp"
+#include "aScoreManager.h"
+
 
 class aHammerAction : public Action
 {
 public:
-    float effectRange = 10.0f;
+    float effectRange = 7.5f;
     bool isOn = true;
+    aScoreManager* scoreManager;
 
     void Update() override {
         if (!isOn) return;
@@ -16,6 +19,8 @@ public:
             if (target->name == "Barrel" || target->name == "Flame_Enemy" || target->name == "BlueBarrel") {
                 if (glm::distance(object->mesh->positionXYZ, target->mesh->positionXYZ) <= effectRange) {
                     target->Destroy();
+                    if (scoreManager)
+                        scoreManager->DrawScore(target->mesh->positionXYZ)
                 }
             }
         }
