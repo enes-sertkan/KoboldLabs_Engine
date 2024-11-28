4bd4c7887decdd41829141707db88eddcc4c3004
VasukaLabs <vasukaman@gmail.com>
2024-11-27 21:53:10 -0400

    Added aIconNearObject

---
+++ NoodleNexus/NoodleNexus/aIconNearObject.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Action.h"
+#include "Scene.hpp"
+#include "aRayCastPhysics2D.hpp"
+#include "aModelsFramesAnimator.hpp"
+#include "aRayCastPhysics2D.hpp"
+
+class aIconNearObject : public Action
+{
+public:
+
+    std::string modelName;
+    bool isOn = false;
+    Object* objectToFollow = new Object();
+    glm::vec3 offset;
+
+
+    void Update() override
+    {
+
+     if (isOn)
+     {
+         object->mesh->modelFileName = modelName;
+         object->mesh->positionXYZ = objectToFollow->mesh->positionXYZ+offset;
+         object->mesh->bIsVisible = true;
+     }
+     else
+     {
+         object->mesh->bIsVisible = false;
+     }
+    }
+};
